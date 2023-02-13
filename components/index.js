178b4b1a5a648f2f5e5b6f9ccd693be57c195815
amro98 <amrohossam1160@gmail.com>
2023-02-14 00:57:37 +0200

    first commit

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { Circlebutton,Rectbutton } from "./Button";
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    Circlebutton,
+    Rectbutton
+}
